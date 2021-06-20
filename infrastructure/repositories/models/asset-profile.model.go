08fe306af5bc0718b36ebfddc160657f9184b217
le.trong <le.trong>
2021-06-20 16:07:50 -0400

    Updated model

--- infrastructure/repositories/models/asset-profile.model.go
+++ infrastructure/repositories/models/asset-profile.model.go
@@ -13,8 +13,8 @@ type AssetProfileModel struct {
 	ID         *primitive.ObjectID `bson:"_id,omitempty"`
 	CreatedAt  int64               `bson:"createdAt,omitempty"`
 	ModifiedAt int64               `bson:"modifiedAt,omitempty"`
-	Enabled    bool                `bson:"enabled,omitempty"`
-	Deleted    bool                `bson:"deleted,omitempty"`
+	Enabled    bool                `bson:"enabled"`
+	Deleted    bool                `bson:"deleted"`
 	Schema     string              `bson:"schema,omitempty"`
 	Ticker     string              `bson:"ticker,omitempty"`
 	Sector     string              `bson:"sector,omitempty"`
